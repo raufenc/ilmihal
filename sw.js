c6a5745679043b57fa47c6ae71eb069892df8c6d
Rauf <raufenc@gmail.com>
2026-03-26 07:54:31 +0300

    SW cache versiyon senkronize (v10, css v21, app v28)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - ilmihal.org PWA
-var CACHE_NAME = 'ilmihal-v9';
+var CACHE_NAME = 'ilmihal-v10';
 var CORE_ASSETS = [
   '/',
   '/index.html',
@@ -13,7 +13,7 @@ var CORE_ASSETS = [
   '/audio-map.js?v=1',
   '/arama-sozluk.js?v=1',
   '/rehberler.js?v=1',
-  '/app.js?v=25',
+  '/app.js?v=28',
   '/favicon.svg',
   '/manifest.json'
 ];
